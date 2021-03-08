a8711c15fa94e952162a6ff171f4f6659d5b3a2a
B <xxflamegamingxx@gmail.com>
2021-03-08 08:50:21 -0500

    Added temp 3d setup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,21 @@
 
 #include <QtCharts/QSplineSeries>
 #include <QtCharts/QChartView>
-#include <QtDataVisualization/Q3DSurface>
+
+#include <Qt3DCore/QEntity>
+#include <Qt3DRender/QCamera>
+#include <Qt3DRender/QCameraLens>
+#include <Qt3DCore/QTransform>
+#include <Qt3DCore/QAspectEngine>
+
+#include <Qt3DInput/QInputAspect>
+
+#include <Qt3DRender/QRenderAspect>
+#include <Qt3DExtras/QForwardRenderer>
+#include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DExtras/QCylinderMesh>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/QOrbitCameraController>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -125,6 +139,45 @@ MainWindow::MainWindow(QWidget *parent)
     ui->kinematicsGraphView->setStyleSheet(NULL);
     ui->kinematicsGraphView->setRenderHint(QPainter::Antialiasing);
 
+    //3D Stuffs
+    Qt3DCore::QEntity *scene = new Qt3DCore::QEntity();
+    Qt3DRender::QMaterial *material = new Qt3DExtras::QPhongMaterial(scene);
+
+    Qt3DCore::QEntity *cylinderEntity = new Qt3DCore::QEntity(scene);
+    Qt3DExtras::QCylinderMesh *cylinderMesh = new Qt3DExtras::QCylinderMesh;
+    cylinderMesh->setRadius(5);
+    cylinderMesh->setLength(10);
+
+    cylinderMesh->setRings(4);
+    cylinderMesh->setSlices(10);
+
+    //Transform cylinder
+    Qt3DCore::QTransform *cylinderTransform = new Qt3DCore::QTransform;
+    cylinderTransform->setScale3D(QVector3D(1, 1, 1)); //Keep scaled at typical for now
+    cylinderTransform->setRotation(QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), 45.0f));
+
+    //Add configurations to cylinder
+    cylinderEntity->addComponent(cylinderMesh);
+    cylinderEntity->addComponent(cylinderTransform);
+    cylinderEntity->addComponent(material);
+
+    Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
+    Qt3DRender::QCamera *camera = view->camera();
+    camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
+    camera->setPosition(QVector3D(0, 0, 40.0f));
+    camera->setViewCenter(QVector3D(0, 0, 0));
+
+    Qt3DExtras::QOrbitCameraController *cameraController = new Qt3DExtras::QOrbitCameraController(scene);
+    cameraController->setLinearSpeed(50.0f);
+    cameraController->setLookSpeed(180.0f);
+    cameraController->setCamera(camera);
+
+    view->setRootEntity(scene);
+
+    QWidget *container = QWidget::createWindowContainer(view);
+
+    //Get rid of unnessary horizontal layout
+    ui->simulationWidgetPlaceHolder->parentWidget()->layout()->replaceWidget(ui->simulationWidgetPlaceHolder, container);
 }
 
 //MainWindow deconstructor
