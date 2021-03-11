2a55bab7f098ff9448cfa314eaab7cfd1c3749a1
B <xxflamegamingxx@gmail.com>
2021-03-10 21:10:29 -0500

    little bit of refactoring

--- gamepadhandler.cpp
+++ gamepadhandler.cpp
@@ -18,6 +18,50 @@ GamepadHandler::GamepadHandler()
 }
 
 
+// Setters
+/**
+ * @brief Sets current gamepad to desired position
+ * @param Starts at 0; 0 being 1st device in list, etc.
+ * @return True if gamepad was successfully set, otherwise false.
+ */
+bool GamepadHandler::setCurrentGamepad(int deviceIDPos)
+{
+    qDebug() << "Attemping to set gamepad to device #:" << deviceIDPos << "...";
+    if (!(gamepadList->isEmpty()) && (getTotalConnected() == deviceIDPos + 1)) {
+        currentGamepad = new QGamepad(gamepadList->at(deviceIDPos));
+        currentGamepadIDPos = deviceIDPos;
+        qDebug() << "Successfully set gamepad to device #:" << deviceIDPos << "!";
+        qDebug() << "Current gamepad ID position:" << currentGamepadIDPos;
+        return true;
+    } else {
+        qDebug() << "Failed to set gamepad to device #:" << deviceIDPos << "!";
+        qDebug() << "Current gamepad ID positon:" << currentGamepadIDPos;
+        return false;
+    }
+}
+
+
+// Getters
+/**
+ * @brief Gets current gamepad instance.
+ * @return QGamepad instance.
+ */
+QGamepad* GamepadHandler::getCurrentGamepad()
+{
+    return currentGamepad;
+}
+
+/**
+ * @brief Gets total of connected devices
+ * @return Number of connected devices
+ */
+int GamepadHandler::getTotalConnected()
+{
+    return gamepadList->length();
+}
+
+
+// Methods
 /**
  * @brief Updates the gamepad list with currently connected devices.
  * @return True if list contains gamepads, otherwise false.
@@ -41,8 +85,8 @@ bool GamepadHandler::updateGamepadList()
 
 
 /**
- * @brief Updates the gamepad list, and reset current gamepad to desired position.
- * Will also configure slots and connections.
+ * @brief Updates the gamepad list, and reset current gamepad to
+ * desired position. Will also configure slots and connections.
  * @return True if all operations were successful, otherwise false.
  */
 bool GamepadHandler::refreshGamepad()
@@ -62,114 +106,116 @@ bool GamepadHandler::refreshGamepad()
  */
 void GamepadHandler::configureConnections()
 {
-    connect(currentGamepad, &QGamepad::axisLeftXChanged, this, [](double value){
+    connect(currentGamepad, &QGamepad::axisLeftXChanged,
+            this, [this](double value){
         qDebug() << "Left X" << value;
+        emit gamepad_axisLeftXChanged(value);
     });
-    connect(currentGamepad, &QGamepad::axisLeftYChanged, this, [](double value){
+    connect(currentGamepad, &QGamepad::axisLeftYChanged,
+            this, [this](double value){
         qDebug() << "Left Y" << value;
+        emit gamepad_axisLeftYChanged(value);
     });
-    connect(currentGamepad, &QGamepad::axisRightXChanged, this, [](double value){
+    connect(currentGamepad, &QGamepad::axisRightXChanged,
+            this, [this](double value){
         qDebug() << "Right X" << value;
+        emit gamepad_axisRightXChanged(value);
     });
-    connect(currentGamepad, &QGamepad::axisRightYChanged, this, [](double value){
+    connect(currentGamepad, &QGamepad::axisRightYChanged,
+            this, [this](double value){
         qDebug() << "Right Y" << value;
+        emit gamepad_axisRightYChanged(value);
     });
-    connect(currentGamepad, &QGamepad::buttonAChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonAChanged,
+            this, [this](bool pressed){
         qDebug() << "Button A" << pressed;
+        emit gamepad_buttonAChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonBChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonBChanged,
+            this, [this](bool pressed){
         qDebug() << "Button B" << pressed;
+        emit gamepad_buttonBChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonXChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonXChanged,
+            this, [this](bool pressed){
         qDebug() << "Button X" << pressed;
+        emit gamepad_buttonXChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonYChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonYChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Y" << pressed;
+        emit gamepad_buttonYChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonL1Changed, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonL1Changed,
+            this, [this](bool pressed){
         qDebug() << "Button L1" << pressed;
+        emit gamepad_buttonL1Changed(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonR1Changed, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonR1Changed,
+            this, [this](bool pressed){
         qDebug() << "Button R1" << pressed;
+        emit gamepad_buttonR1Changed(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonL2Changed, this, [](double value){
+    connect(currentGamepad, &QGamepad::buttonL2Changed,
+            this, [this](double value){
         qDebug() << "Button L2: " << value;
+        emit gamepad_buttonL2Changed(value);
     });
-    connect(currentGamepad, &QGamepad::buttonR2Changed, this, [](double value){
+    connect(currentGamepad, &QGamepad::buttonR2Changed,
+            this, [this](double value){
         qDebug() << "Button R2: " << value;
+        emit gamepad_buttonR2Changed(value);
     });
-    connect(currentGamepad, &QGamepad::buttonL3Changed, this, [](double value){
-        qDebug() << "Button L3: " << value;
+    connect(currentGamepad, &QGamepad::buttonL3Changed,
+            this, [this](bool pressed){
+        qDebug() << "Button L3: " << pressed;
+        emit gamepad_buttonL3Changed(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonR3Changed, this, [](double value){
-        qDebug() << "Button R3: " << value;
+    connect(currentGamepad, &QGamepad::buttonR3Changed,
+            this, [this](bool pressed){
+        qDebug() << "Button R3: " << pressed;
+        emit gamepad_buttonR3Changed(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonSelectChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonSelectChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Select" << pressed;
+        emit gamepad_buttonSelectChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonStartChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonStartChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Start" << pressed;
+        emit gamepad_buttonStartChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonGuideChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonGuideChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Guide" << pressed;
+        emit gamepad_buttonGuideChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonCenterChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonCenterChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Center" << pressed;
+        emit gamepad_buttonCenterChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonUpChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonUpChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Up" << pressed;
+        emit gamepad_buttonUpChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonDownChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonDownChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Down" << pressed;
+        emit gamepad_buttonDownChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonLeftChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonLeftChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Left" << pressed;
+        emit gamepad_buttonLeftChanged(pressed);
     });
-    connect(currentGamepad, &QGamepad::buttonRightChanged, this, [](bool pressed){
+    connect(currentGamepad, &QGamepad::buttonRightChanged,
+            this, [this](bool pressed){
         qDebug() << "Button Right" << pressed;
+        emit gamepad_buttonRightChanged(pressed);
     });
-}
-
-
-// Setters
-/**
- * @brief Sets current gamepad to desired position
- * @param Starts at 0; 0 being 1st device in list, etc.
- * @return True if gamepad was successfully set, otherwise false.
- */
-bool GamepadHandler::setCurrentGamepad(int deviceIDPos)
-{
-    qDebug() << "Attemping to set gamepad to device #:" << deviceIDPos << "...";
-    if (!(gamepadList->isEmpty()) && (getTotalConnected() == deviceIDPos + 1)) {
-        currentGamepad = new QGamepad(gamepadList->at(deviceIDPos));
-        currentGamepadIDPos = deviceIDPos;
-        qDebug() << "Successfully set gamepad to device #:"<< deviceIDPos << "!";
-        qDebug() << "Current gamepad ID position:" << currentGamepadIDPos;
-        return true;
-    } else {
-        qDebug() << "Failed to set gamepad to device #:"<< deviceIDPos << "!";
-        qDebug() << "Current gamepad ID positon:" << currentGamepadIDPos;
-        return false;
-    }
-}
-
-// Getters
-/**
- * @brief Gets current gamepad instance.
- * @return QGamepad instance.
- */
-QGamepad* GamepadHandler::getCurrentGamepad()
-{
-    return currentGamepad;
-}
-
-/**
- * @brief Gets total of connected devices
- * @return Number of connected devices
- */
-int GamepadHandler::getTotalConnected()
-{
-    return gamepadList->length();
 }
 
 
--- gamepadhandler.h
+++ gamepadhandler.h
@@ -18,6 +18,31 @@ public:
 public slots:
     bool refreshGamepad();
 
+signals:
+    void gamepad_axisLeftXChanged(double);
+    void gamepad_axisLeftYChanged(double);
+    void gamepad_axisRightXChanged(double);
+    void gamepad_axisRightYChanged(double);
+    void gamepad_buttonAChanged(bool);
+    void gamepad_buttonBChanged(bool);
+    void gamepad_buttonXChanged(bool);
+    void gamepad_buttonYChanged(bool);
+    void gamepad_buttonL1Changed(bool);
+    void gamepad_buttonR1Changed(bool);
+    void gamepad_buttonL2Changed(double);
+    void gamepad_buttonR2Changed(double);
+    void gamepad_buttonL3Changed(bool);
+    void gamepad_buttonR3Changed(bool);
+    void gamepad_buttonSelectChanged(bool);
+    void gamepad_buttonStartChanged(bool);
+    void gamepad_buttonGuideChanged(bool);
+    void gamepad_buttonCenterChanged(bool);
+    void gamepad_buttonUpChanged(bool);
+    void gamepad_buttonDownChanged(bool);
+    void gamepad_buttonLeftChanged(bool);
+    void gamepad_buttonRightChanged(bool);
+
+
 private:
     QGamepad *currentGamepad;
     QGamepadManager *gamepadManager;
