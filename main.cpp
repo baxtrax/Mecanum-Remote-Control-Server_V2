2503d12b99960acf1321a0c74a53843a70bbe3a5
baxtrax <34373485+baxtrax@users.noreply.github.com>
2022-03-25 20:41:17 -0400

    Modified multiple letter-spacing to match Figma design

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     QFontDatabase::addApplicationFont(":/fonts/resources/OpenSans-ExtraBold.ttf");
     QFontDatabase::addApplicationFont(":/fonts/resources/OpenSans-Regular.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/resources/OpenSans-Light.ttf");
     w.show();
     return a.exec();
 }
